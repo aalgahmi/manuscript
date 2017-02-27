368f38a682a488283de92ba931e58e638b219a25
Abdulmalek Al-Gahmi <aalgahmi@BFMACBK-006.local>
2017-02-27 13:36:27 -0700

    Preloaded config

--- test/manuscript_test.rb
+++ test/manuscript_test.rb
@@ -9,23 +9,24 @@ require "#{APP_ROOT}/lib/manuscript/compiler/base"
 class ManuscriptTest < Minitest::Test
   def setup 
     FileUtils.rm_rf Dir.glob("#{APP_ROOT}/output/*")
+    @config = YAML.load_file("#{APP_ROOT}/config/application.yml")['manuscript']['compiler']
   end
   
   def test_html_compiler
     require "#{APP_ROOT}/lib/manuscript/compiler/html"
     Manuscript::Compiler::Html.new(:default).compile
-    assert File.exist?("#{APP_ROOT}/output/manuscript.html")
+    assert File.exist?("#{APP_ROOT}/output/#{@config['output']}.html")
   end
   
   def test_latex_compiler
     require "#{APP_ROOT}/lib/manuscript/compiler/latex"
     Manuscript::Compiler::Latex.new(:default).compile
-    assert File.exist?("#{APP_ROOT}/output/manuscript.tex")
+    assert File.exist?("#{APP_ROOT}/output/#{@config['output']}.tex")
   end
   
   def test_epub_compiler
     require "#{APP_ROOT}/lib/manuscript/compiler/epub"
     Manuscript::Compiler::Epub.new(:default).compile
-    assert File.exist?("#{APP_ROOT}/output/manuscript.epub")
+    assert File.exist?("#{APP_ROOT}/output/#{@config['output']}.epub")
   end
 end
